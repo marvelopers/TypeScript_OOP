10ad13695e0baa1c3c87a71d3329665b88b38873
marvelopers <marvelopers@gmail.com>
2021-02-14 12:38:11 +0900

    Add stack Array 구현

--- 04_Stack/index.ts
+++ 04_Stack/index.ts
@@ -3,3 +3,45 @@ interface Stack {
   push(value: string): void;
   pop(value: string): void;
 }
+type StackNode = {
+  readonly value: string;
+  readonly next?: StackNode;
+}
+
+//단일 연결 리스트 이용
+
+class StackImpl implements Stack {
+  private _size: number = 0;
+  private head?: StackNode;
+
+  constructor(private capacity: number) { }
+  get size() {
+    return this._size;
+  }
+  push(value: string) {
+    if (this._size === this.capacity) {
+      throw new Error('Stack is full ')
+    }
+    const node: StackNode = { value, next: this.head }
+    this.head = node;
+    this._size++;
+  }
+  pop(): string {
+    if (this.head == null) {
+      throw new Error('Stack is empty')
+    }
+    const node = this.head;
+    this.head = node.next;
+    this._size--;
+
+    return node.value;
+  }
+}
+
+const stack = new StackImpl(10);
+stack.push('chloe_01');
+stack.push('chloe_02');
+stack.push('chloe_03');
+while (stack.size !== 0) {
+  console.log(stack.pop())
+}
